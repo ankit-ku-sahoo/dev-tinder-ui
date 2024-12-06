73cfd450ab3f8ddff239521bc3478a5f037858e8
Ankit Kumar Sahoo <10ankitkusahoo10@gmail.com>
2024-12-06 12:45:08 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter basename='/'>
-      <Routes>
-        <Route path='/' element={<Body/>}>
-          <Route path='/login' element={<div>Login Page</div>} />
-          <Route path='/feed' element={<div>Feed Page</div>} />
-          <Route path='/profile' element={<div>Profile Page</div>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter basename='/'>
+    //   <Routes>
+    //     <Route path='/' element={<Body/>}>
+    //       <Route path='/login' element={<div>Login Page</div>} />
+    //       <Route path='/feed' element={<div>Feed Page</div>} />
+    //       <Route path='/profile' element={<div>Profile Page</div>} />
+    //     </Route>
+    //   </Routes>
+    // </BrowserRouter>
+    <>Hello World!</>
   );
 }
 
