a115d36447cea444f9db909f04673a0e63d36fdf
Ankit Kumar Sahoo <10ankitkusahoo10@gmail.com>
2024-12-06 13:03:47 +0530

    reverted changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import { useState } from 'react';
 import './App.css';
-// import Body from './Body';
+import Body from './Body';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    // <BrowserRouter basename='/'>
-    //   <Routes>
-    //     <Route path='/' element={<Body/>}>
-    //       <Route path='/login' element={<div>Login Page</div>} />
-    //       <Route path='/feed' element={<div>Feed Page</div>} />
-    //       <Route path='/profile' element={<div>Profile Page</div>} />
-    //     </Route>
-    //   </Routes>
-    // </BrowserRouter>
-    <>Hello World!</>
+    <BrowserRouter basename='/'>
+      <Routes>
+        <Route path='/' element={<Body/>}>
+          <Route path='/login' element={<div>Login Page</div>} />
+          <Route path='/feed' element={<div>Feed Page</div>} />
+          <Route path='/profile' element={<div>Profile Page</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
